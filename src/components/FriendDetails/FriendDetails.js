c9f5f30a4c521a2652ffbc165b7348171ebd6431
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-29 18:43:55 +0600

    add dynamic friendDetails data

--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
+    const [friend, setFriend] = useState({})
     const { friendId } = useParams()
+    const { name, address, email, phone } = friend;
+    console.log(friend);
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
     return (
         <div>
-            <h1>This is Friends Details id: {friendId}</h1>
+            <h1>This is Friends Details</h1>
+            <h3>Name: {name}</h3>
+            <h4>Phone: {phone}</h4>
+            <h4>Email: {email}</h4>
+            <h5>City: {address?.city} Geo: {address?.geo.lng}</h5>
         </div>
     );
 };
